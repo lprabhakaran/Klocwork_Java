552938a97a0d82a0c130b5c75aa9da97f14a7bc7
Lekha Prabhakaran <lprabhakaran@cloudbees.com>
2025-11-08 22:09:52 +0530

    add c repo redis

---
+++ deps/xxhash/cachedObjs/cbad4ce87f68cd06aa92ff37a641d079/xxhash.d
@@ -0,0 +1,2 @@
+cachedObjs/cbad4ce87f68cd06aa92ff37a641d079/xxhash.o: xxhash.c xxhash.h
+xxhash.h:
---
+++ src/release.h
@@ -0,0 +1,5 @@
+#define REDIS_GIT_SHA1 "e4dda44d"
+#define REDIS_GIT_DIRTY "       0"
+#define REDIS_BUILD_ID "lprabhakaran-MacBook-Pro-1762619676"
+#include "version.h"
+#define REDIS_BUILD_ID_RAW REDIS_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
